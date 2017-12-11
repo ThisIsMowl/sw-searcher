0d48a23a6c830712ffbd9546d454d58795ce1854
MDonald1 <m.m.donald@outlook.com>
2017-12-11 11:26:08 +0000

    Provider set up, added sample HelloWorld component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import common from './actions/commonActions'
+import HelloWorld from './components/HelloWorld'
+import logo from './logo.svg'
+import './App.css'
+
+const mapState = state => ({
+  ...state.common,
+})
+
+const mapDispatch = dispatch => ({
+  appLoad: () =>
+    dispatch(common.appLoaded())
+})
 
 class App extends Component {
+  componentWillMount() {
+    this.props.appLoad()
+  }
+
   render() {
+    const appLoaded = this.props.appLoaded
+
     return (
       <div className="App">
         <header className="App-header">
@@ -13,9 +32,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <HelloWorld working={appLoaded}/>
       </div>
     );
   }
 }
 
-export default App;
+export default connect(mapState, mapDispatch)(App)
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,9 @@
+const HelloWorld = (props) => {
+  if (props.working) {
+    return (
+      <h1>Hello World</h1>
+    )
+  } else return null
+}
+
+export default HelloWorld
