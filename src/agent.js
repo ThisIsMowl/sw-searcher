6d28d1f1203168623bd0940a0c0f7eed157ed805
MDonald1 <m.m.donald@outlook.com>
2017-12-13 08:34:47 +0000

    Agent file now handles get requests

--- src/agent.js
+++ src/agent.js
@@ -2,13 +2,31 @@
 
 import _superagent from 'superagent'
 import _promise from 'superagent-promise'
-import { bluebird as Promise } from 'bluebird'
+import bluebird from 'bluebird'
 
-const superagent = _promise(_superagent, Promise)
+const superagent = _promise(_superagent, bluebird)
 
 const API_ROOT = 'https://swapi.co/api/'
 
+const responseBody = res => res.body
+
 const requests = {
   get: url =>
-    superagent.get(`${API_ROOT}${url}`)
+    superagent.get(`${API_ROOT}${url}`).then(responseBody),
+}
+
+const getShip = value => requests.get(`starships/${value}`)
+const getCharacter = value => requests.get(`people/${value}`)
+const getFilm = value => requests.get(`films/${value}`)
+const getPlanet = value => requests.get(`planets/${value}`)
+const getVehicle = value => requests.get(`vehicles/${value}`)
+const getSpecies = value => requests.get(`species/${value}`)
+
+export default {
+  getShip,
+  getCharacter,
+  getFilm,
+  getPlanet,
+  getVehicle,
+  getSpecies,
 }
