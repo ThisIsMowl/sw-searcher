d64f9bb80ee5dfd500c787fbb125b489dbd89316
MDonald1 <m.m.donald@outlook.com>
2018-01-22 10:57:33 +0000

    Placeholder logic for ResultsPanel

--- src/components/ResultsPanels/ResultsPanel.js
+++ src/components/ResultsPanels/ResultsPanel.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const ResultsPanel = props => (
-  <div className="content">
-  </div>
-)
+const mapState = state => ({
+  data: state.common.data,
+  type: state.common.searchType,
+})
 
-export default ResultsPanel
+const ResultsPanel = (props) => {
+  if (props.data && props.type) {
+    switch (props.type) {
+      case 'planet':
+        return (<h4>Planet</h4>)
+      case 'film':
+        return (<h4>Film</h4>)
+      case 'species':
+        return (<h4>Species</h4>)
+      case 'starship':
+        return (<h4>Starship</h4>)
+      case 'vehicle':
+        return (<h4>Vehicle</h4>)
+      case 'character':
+        return (<h4>Character</h4>)
+      default:
+        return null
+    }
+  }
+  return null
+}
+
+export default connect(mapState, () => ({}))(ResultsPanel)
