6aac01b9ad22aad3b6c7536699984e3e44647ccd
MDonald1 <m.m.donald@outlook.com>
2018-02-01 10:29:37 +0000

    Small fix for helper fuctions

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -8,12 +8,14 @@ const toRoman = (x) => {
   if (x) {
     return numerals.indexOf(x) > -1 ? epNumbers[numerals.indexOf(x)] : ''
   }
+  return null
 }
 
 const capCase = (x) => {
   if (x) {
     return x.charAt(0).toUpperCase() + x.slice(1)
   }
+  return null
 }
 
 export default {
