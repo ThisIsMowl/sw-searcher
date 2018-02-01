4515860eca10cca1cbe032e4db72085ea028f3f5
MDonald1 <m.m.donald@outlook.com>
2018-02-01 11:24:14 +0000

    Fix redundancy

--- src/components/SearchPanel/SearchResultsHolder.js
+++ src/components/SearchPanel/SearchResultsHolder.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import resultListActions from '../../actions/resultsListActions'
 import common from '../../actions/commonActions'
 import agent from '../../agent'
 
