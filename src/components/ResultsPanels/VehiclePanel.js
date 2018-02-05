3bd1b0b8b6bbb46ad5d12d522d43cb6de31d7b6d
MDonald1 <m.m.donald@outlook.com>
2018-02-05 10:32:09 +0000

    Improvements to VehiclePanel

--- src/components/ResultsPanels/VehiclePanel.js
+++ src/components/ResultsPanels/VehiclePanel.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Numeral from 'numeral'
 
+import helpers from '../../helpers/helpers'
+
 const VehiclePanel = (props) => {
   // Destructure
   const {
@@ -8,13 +10,13 @@ const VehiclePanel = (props) => {
     consumables,
     cost_in_credits: cost,
     crew,
-    hyperdrive_rating: hyperRating,
     length,
     manufacturer,
     max_atmosphering_speed: atmoSpeed,
     model,
     name,
     passengers,
+    vehicle_class: vehicleClass,
   } = props.data
 
 
@@ -24,14 +26,14 @@ const VehiclePanel = (props) => {
         <h3>{name}</h3>
         <h4>Length: {Numeral(length).format()}m</h4>
         <h4>Crew: {Numeral(crew).format()}</h4>
-        <h4>Passenger(s): {Numeral(passengers).format()}</h4>
+        <h4>No. of Passengers: {Numeral(passengers).format()}</h4>
+        <h4>Class: {helpers.capCase(vehicleClass)}</h4>
         <h4>Model: {model}</h4>
         <h4>Manufacturer: {manufacturer}</h4>
         <h4>Cargo Capacity: {Numeral(capacity).format()}</h4>
-        <h4>Speed: {atmoSpeed}</h4>
-        <h4>Hyperdrive Rating: {hyperRating}</h4>
-        <h4>Cost: {cost} credits</h4>
-        <h4>Consumables: {consumables}</h4>
+        <h4>Speed: {atmoSpeed}km/h</h4>
+        <h4>Cost: {cost === 'unknown' ? 'Unknown' : `${cost} credits`}</h4>
+        <h4>Max. Consumable Supply: {helpers.capCase(consumables)}</h4>
       </div>
     )
   }
